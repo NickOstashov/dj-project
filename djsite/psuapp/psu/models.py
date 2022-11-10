096f41ad19f2d55c2b1e8d011c321d9f99d04eef
NickOstashov <ostashiv@gmail.com>
2022-11-11 01:14:05 +0500

    add icon

--- djsite/psuapp/psu/models.py
+++ djsite/psuapp/psu/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Problem(models.Model):
     naming = models.CharField(max_length=50, verbose_name = "Наименование возможной проблемы")
     description= models.CharField(max_length = 255, verbose_name = "Описание возможной проблемы")
-    icon = models.ImageField(blank=True, null = True, upload_to='psuapp/media/images', verbose_name="Изображение")
+    icon = models.ImageField(blank=True, null = True, upload_to='images', verbose_name="Изображение")
 
     def __str__(self):
         return self.naming
